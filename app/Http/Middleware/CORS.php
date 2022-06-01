fed99a0fc2b07f88ffdb40f55f5798f14e047733
codecommit <rana.buddy@gmail.com>
2022-06-02 00:55:37 +0600

    added

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -16,31 +16,23 @@ class CORS
      */
     public function handle(Request $request, Closure $next)
     {
-        if(isset($_SERVER["HTTP_ORIGIN"]))
-{
-    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-}
-else
-{
-    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
-    header("Access-Control-Allow-Origin: *");
-}
-
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 600");    // cache for 10 minutes
-
-if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
-{
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
-        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
-
-    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    //Just exit with 200 OK with the above headers for OPTIONS method
-    exit(0);
-}
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+            header('Access-Control-Allow-Headers: token, Content-Type');
+            header('Access-Control-Max-Age: 1728000');
+            header('Content-Length: 0');
+            header('Content-Type: text/plain');
+            die();
+        }
+    
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+    
+        $ret = [
+            'result' => 'OK',
+        ];
+        print json_encode($ret);
         return $next($request);
     }
 }
