8ecc60e984dcc4abd7968db658f46c8081944c20
codecommit <rana.buddy@gmail.com>
2022-06-01 23:33:50 +0600

    added

---
+++ app/Http/Middleware/CORS.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CORS
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: X-Requested-With, Content-Type,X-Auth-Token, Origin,boundary,charset, Content-Length,Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+        header("Access-Control-Allow-Methods: PUT, POST, GET,PATCH, OPTIONS, DELETE");
+        return $next($request);
+    }
+}
