e46ba378ec6d37fc10c63d55aaab3b9b001f6e4e
codecommit <rana.buddy@gmail.com>
2022-06-01 23:56:03 +0600

    added

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -16,9 +16,10 @@ class CORS
      */
     public function handle(Request $request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: X-Requested-With, Content-Type,X-Auth-Token, Origin,boundary,charset, Content-Length,Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
         header("Access-Control-Allow-Methods: PUT, POST, GET,PATCH, OPTIONS, DELETE");
+        header("Access-Control-Allow-Headers: X-Requested-With, Content-Type,X-Auth-Token, Origin,boundary,charset, Content-Length,Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+        header("Access-Control-Allow-Origin: *");
+
         return $next($request);
     }
 }
