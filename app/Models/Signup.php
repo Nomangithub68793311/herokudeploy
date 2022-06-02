5657ac0b39f582ac5ed97478f75a6de06f0e4f5b
codecommit <rana.buddy@gmail.com>
2022-06-02 16:25:05 +0600

    updated

--- app/Models/Signup.php
+++ app/Models/Signup.php
@@ -5,9 +5,13 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Signup extends Model
+class Signup extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     protected $dateFormat = 'Y-m-d';
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
 }
--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'customer' => [
+            'driver' => 'sanctum',
+            'provider' => 'signups',
+        ],
     ],
 
     /*
@@ -63,6 +67,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'signups' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Signup::class,
         ],
 
         // 'users' => [
