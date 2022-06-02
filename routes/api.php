480899840c18d247c3c59427de86906dcb47cb39
codecommit <rana.buddy@gmail.com>
2022-06-02 13:52:05 +0600

    updated

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\BioController;
-use \App\Http\Middleware\checkuser;
+use App\Http\Middleware\checkuser;
 
 /*
 |--------------------------------------------------------------------------
