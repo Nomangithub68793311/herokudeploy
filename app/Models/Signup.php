a5445520b53bac9d27fe5273c6dc9c581e25f4db
codecommit <rana.buddy@gmail.com>
2022-06-02 16:40:22 +0600

    updated

--- app/Models/Signup.php
+++ app/Models/Signup.php
@@ -4,15 +4,15 @@ namespace App\Models;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\Signup as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Signup extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     protected $dateFormat = 'Y-m-d';
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
 
 }
